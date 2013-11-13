c7d343436532f88511dd68d1a60ec97dd5ff4597
Gregg Horton <greggahorton@gmail.com>
2013-11-12 18:30:42 -0800

    installed devise

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,4 +77,8 @@ Pintresting::Application.configure do
 
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
+
+  #set to hostname
+  config.action_mailer.default_url_options = { :host => 'pintresting-gregg.herokuapp.com' }
+
 end
