e44a8bf511dd009464cce8a2cfea7e56df12d5b2
Gregg Horton <greggahorton@gmail.com>
2013-11-14 19:23:30 -0800

    adding aws for paperclip for heroku

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -78,7 +78,16 @@ Pintresting::Application.configure do
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
 
-  #set to hostname
+  # set to hostname
   config.action_mailer.default_url_options = { :host => 'pintresting-gregg.herokuapp.com' }
 
+  # paperclip uploads images to s3
+  config.paperclip_defaults = {
+  :storage => :s3,
+  :s3_credentials => {
+    :bucket => ENV['AWS_BUCKET'],
+    :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
+    :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
+  }
+}
 end
